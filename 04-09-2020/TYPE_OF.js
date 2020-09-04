2974bf54ad77b6b020fc22fd3402ec008713bc30
rushik1995 <68633315+rushik1995@users.noreply.github.com>
2020-09-04 23:22:40 +0530

    Delete TYPE_OF.js

--- 04-09-2020/TYPE_OF.js
+++
@@ -1,8 +0,0 @@
-console.log(typeof(1));
-console.log(typeof(1.1));
-console.log(typeof("1.1"));
-console.log(typeof(true));
-console.log(typeof(null));
-console.log(typeof(undefined));
-console.log(typeof([]));
-console.log(typeof({})); 
